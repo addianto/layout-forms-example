ca5a5b686eeebef695a0c5fb51ef421e10bd6fb1
Daya Adianto <dayaadianto@cs.ui.ac.id>
2025-10-31 20:19:22 +0700

    feat: Stub the new page

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,10 @@ class LayoutsFormsExample extends StatelessWidget {
         useMaterial3: true,
       ),
       initialRoute: '/',
-      routes: {'/': (context) => const OriginalExamplePage()},
+      routes: {
+        '/': (context) => const OriginalExamplePage(),
+        '/more-widget-examples': (context) => const MoreWidgetExamplesPage(),
+      },
     );
   }
 }
@@ -52,9 +55,67 @@ class OriginalExamplePage extends StatelessWidget {
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 16),
             ),
+            const SizedBox(height: 32),
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, '/more-widget-examples');
+              },
+              label: const Text('More Widget Examples'),
+              icon: const Icon(Icons.arrow_forward),
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class MoreWidgetExamplesPage extends StatelessWidget {
+  const MoreWidgetExamplesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('More Widget Examples'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: const <Widget>[
+          ExampleSection(title: '1. Container', child: Text('')),
+          ExampleSection(title: '2. Expanded & Flexible', child: Text('')),
+          ExampleSection(title: '3. Stack', child: Text('')),
+          ExampleSection(title: '4. GridView', child: Text('')),
+        ],
+      ),
+    );
+  }
+}
+
+class ExampleSection extends StatelessWidget {
+  final String title;
+  final Widget child;
+
+  const ExampleSection({super.key, required this.title, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(title, style: Theme.of(context).textTheme.headlineSmall,),
+        const SizedBox(height: 16),
+        Container(
+          padding: const EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black26),
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: child,
+        ),
+        const SizedBox(height: 16),
+      ],
     );
   }
 }
